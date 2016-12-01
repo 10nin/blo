0f6945e9db09180261142a88570bf5bc1dbc8268
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-02 00:38:22 +0900

    Module separate to BloArticle class.

--- blo/__init__.py
+++ blo/__init__.py
@@ -1,34 +1,5 @@
-# -*- coding: utf-8 -*-
-import pathlib
 import sqlite3
 import CommonMark
-from hashlib import sha512
-
-
-class BloArticle:
-    """Article of Blo. written markdown style.
-    """
-    def __init__(self):
-        self.raw_text = ''
-        self.html_text = ''
-        self.hs = sha512()
-
-    def load_from_file(self, file_path):
-        """ The main markdown contents oad from file.
-        :param file_path: path to target markdown file. if file_path is not exists then raise FileNotFoundError.
-        """
-        if pathlib.Path(file_path).exists():
-            with open(file_path) as f:
-                self.raw_text = f.readlines()
-        else:
-            raise FileNotFoundError()
-
-    def convert_to_html(self):
-        pass
-
-    def get_digest(self):
-        self.hs.update(self.html_text.encode('utf-8'))
-        return  self.hs.digest()
 
 
 class Blo:
