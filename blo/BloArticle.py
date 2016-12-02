4b431f9d1715590a0527f57777cc0065814f8cbd
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-02 20:16:47 +0900

    Add html formatter method.
    Convert from markdown text to html text method basic implement.
    TODO: Jinja template apply function.

--- blo/BloArticle.py
+++ blo/BloArticle.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pathlib
+import CommonMark
 from hashlib import sha512
 
 
@@ -7,24 +8,36 @@ class BloArticle:
     """Article of Blo. written markdown style.
     """
     def __init__(self):
-        self.raw_text = ''
-        self.html_text = ''
+        self._raw_text = ''
+        self._html_text = ''
         self.hs = sha512()
 
     def load_from_file(self, file_path):
         """ The main markdown contents oad from file.
+
         :param file_path: path to target markdown file. if file_path is not exists then raise FileNotFoundError.
         """
         if pathlib.Path(file_path).exists():
             with open(file_path) as f:
-                self.raw_text = f.readlines()
+                self._raw_text = f.readlines()
         else:
             raise FileNotFoundError()
 
-    def convert_to_html(self):
-        pass
+    def convert_to_html(self, template_name=""):
+        """ Convert from raw markdown text to html.
+
+        :param template_name: Jinja template file name (find on templates directory), default is empty
+        :return: html formatted text
+        """
+        if template_name == "":
+            self._html_text = CommonMark.commonmark(self._raw_text)
+        else:
+            # TODO: implement generate from markdown to jinja template applied html text
+            pass
+
+        return self._html_text
 
     def get_digest(self):
-        self.hs.update(self.html_text.encode('utf-8'))
+        self.hs.update(self._html_text.encode('utf-8'))
         return  self.hs.digest()
 
