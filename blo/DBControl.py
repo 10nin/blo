97caf640638ad64dfed89e33580e6222d10d809f
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-07 22:00:39 +0900

    Fixed create_tables method bug and test pattern, and add _select_all method.
    _select_all method is query all articles table data, this method for testing.

--- blo/DBControl.py
+++ blo/DBControl.py
@@ -6,13 +6,20 @@ class DBControl:
         self.db_conn = sqlite3.connect(db_name)
 
     def create_tables(self):
-        self.db_conn.execute("""CREATE TABLE IF NOT EXISTS Articles ("
+        c = self.db_conn.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS Articles (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     text TEXT,
                                     digest TEXT UNIQUE,
                                     updatedate TEXT)""")
-        self.db_conn.execute("CREATE VIRTUAL TABLE Articles_fts USING fts4( words TEXT )")
+        c.execute("CREATE VIRTUAL TABLE Articles_fts USING fts4( words TEXT )")
         self.db_conn.commit()
 
     def close_connect(self):
         self.db_conn.close()
+        self.db_conn = None
+
+    def _select_all(self, table_name):
+        c = self.db_conn.cursor()
+        c.execute("SELECT * FROM " + table_name)
+        return c.fetchall()
