d81f27cc646c395ad5c3b9c0c18270d7ed511818
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-08 22:43:44 +0900

    Implement wakachigaki method.

--- blo/Wakachi.py
+++ blo/Wakachi.py
@@ -1,5 +1,12 @@
 import MeCab
+from . import BloArticle
 
 
 class Wakachi:
-    pass
+    def __init__(self, article: BloArticle.BloArticle):
+        self._target_article = article
+
+    def wakachi(self) -> str:
+        mcb = MeCab.Tagger("-Owakati")
+        wakachi_txt = mcb.parse(self._target_article.get_raw_text_body())
+        return wakachi_txt
