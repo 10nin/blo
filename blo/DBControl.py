cbee1e6446c5fdff58f9c9ac17adc6d653af56b3
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-10 01:45:50 +0900

    Add insert article information method and helper utilities.
    Insert from article html, digest, update date, and wakati-gaki style text for full text search.
    and helper utility is get_timestamp method. it return yyyy/mm/dd hh:nn:ss style text data by now local server time.

--- blo/DBControl.py
+++ blo/DBControl.py
@@ -1,11 +1,18 @@
 import sqlite3
+from datetime import datetime
 from .BloArticle import BloArticle
 
 class DBControl:
-    def __init__(self, db_name : str=":memory:"):
+    def __init__(self, db_name: str=":memory:"):
+        """Initializer for blo blog engine database controller.
+
+        :param db_name: database file name connect to it database file. default use on memory database.
+        """
         self.db_conn = sqlite3.connect(db_name)
 
     def create_tables(self):
+        """Create tables for blo blog engine.
+        """
         c = self.db_conn.cursor()
         c.execute("""CREATE TABLE IF NOT EXISTS Articles (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,19 +23,37 @@ class DBControl:
         self.db_conn.commit()
 
     def close_connect(self):
+        """Close database connect.
+        """
         self.db_conn.close()
         self.db_conn = None
 
     def insert_article(self, article: BloArticle):
+        """Insert article html and wakati text to Article/Article_fts tables.
+
+        :param article: target article
+        """
         assert(article is not None)
         # if has not text data then no action on this method.
         if article.has_text :
             c = self.db_conn.cursor()
-            # TODO: Implement insert query for Article and Article_fts
-            c.execute("")
+
+            html = article.get_html()
+            digest = article.get_digest()
+            timestamp = self._get_timestamp()
+            wakati = article.get_wakati_txt()
+
+            c.execute("INSERT INTO Article (text, digest, updatedate) VALUES (?, ?, ?)", (html, digest, timestamp))
+            c.execute("INSERT INTO Article_fts (words) VALUES (?)", (wakati,))
             self.db_conn.commit()
 
-    def _select_all(self, table_name : str) -> list:
+    @staticmethod
+    def _get_timestamp() -> str:
+        """Get timestamp formatted yyyy/mm/dd hh:nn:ss"""
+        return datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+
+    def _select_all(self, table_name: str) -> list:
+        """Select all rows from table_name table. but this method is for testing only."""
         c = self.db_conn.cursor()
         c.execute("SELECT * FROM " + table_name)
         return c.fetchall()
