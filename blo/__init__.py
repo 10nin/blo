21a28af0ed0aa1507dc45fafa128f032a6f2ad3c
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-18 19:03:44 +0900

    Stab implement main routine of Blo.

--- blo/__init__.py
+++ blo/__init__.py
@@ -1,25 +1,14 @@
-import sqlite3
-import CommonMark
+from .BloArticle import BloArticle
 
 
 class Blo:
     def __init__(self):
         pass
 
-    def _open_db_connection(self):
-        pass
-
-    def _convert_to_html(self):
-        pass
-
-    def convert_to_html_str(self):
-        pass
-
-    def _convert_to_wakachi_style(self):
-        pass
-
-    def update_db(self):
-        pass
-
 if __name__ == '__main__':
+    # TODO: implement main routine of Blo.
+    # 1. init database (database name from environment variable or configuration file)
+    # 2. parse markdown file from command line argument.
+    ## if command line argument path is directory then it will do recursive in directory.
+    # 3. generate html and commit to database
     pass
