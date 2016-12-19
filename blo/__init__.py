71f63621e3b7621fa711e417a4f7f76a25b102b9
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-20 01:10:46 +0900

    Add implement memo and configuration file for test.

--- blo/__init__.py
+++ blo/__init__.py
@@ -7,8 +7,11 @@ class Blo:
 
 if __name__ == '__main__':
     # TODO: implement main routine of Blo.
+    # blo [-c config_file] markdown_file.md
+    # -- if no -c option then load config file from default path (current directory).
+    # ---- if no configuration file on current directory blo said error.
     # 1. init database (database name from environment variable or configuration file)
     # 2. parse markdown file from command line argument.
-    ## if command line argument path is directory then it will do recursive in directory.
+    # -- if command line argument path is directory then it will do recursive in directory.
     # 3. generate html and commit to database
     pass
