b1b5d67e49b73e88c8f0bfe6f109bec738882335
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-28 20:22:11 +0900

    Implement main Blo class.

--- blo/__init__.py
+++ blo/__init__.py
@@ -1,6 +1,19 @@
 from blo.BloArticle import BloArticle
+from blo.DBControl import DBControl
 
 
 class Blo:
-    def __init__(self):
-        self.article = BloArticle()
+    def __init__(self, db_file_path, template_dir=""):
+        self.template_dir = template_dir
+        # create tables
+        self.db_file_path = db_file_path
+        self.db_control = DBControl(self.db_file_path)
+        self.db_control.create_tables()
+        self.db_control.close_connect()
+
+    def insert_article(self, file_path):
+        self.db_control = DBControl(self.db_file_path)
+        article = BloArticle(self.template_dir)
+        article.load_from_file(file_path)
+        self.db_control.insert_article(article)
+        self.db_control.close_connect()
