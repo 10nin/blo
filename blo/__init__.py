5422690d03a92096e87a17a2f000abd60bb9ccc8
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-28 20:34:11 +0900

    Change instance variables scope.

--- blo/__init__.py
+++ blo/__init__.py
@@ -7,17 +7,17 @@ class Blo:
     def __init__(self, config_file_path):
         config = configparser.ConfigParser()
         config.read(config_file_path)
-        self.template_dir = config['TEMPLATE']['TEMPLATE_DIR']
-        self.db_file_path = config['DB']['DB_PATH']
+        self._template_dir = config['TEMPLATE']['TEMPLATE_DIR']
+        self._db_file_path = config['DB']['DB_PATH']
 
         # create tables
-        self.db_control = DBControl(self.db_file_path)
-        self.db_control.create_tables()
-        self.db_control.close_connect()
+        self._db_control = DBControl(self._db_file_path)
+        self._db_control.create_tables()
+        self._db_control.close_connect()
 
     def insert_article(self, file_path):
-        self.db_control = DBControl(self.db_file_path)
-        article = BloArticle(self.template_dir)
+        self._db_control = DBControl(self._db_file_path)
+        article = BloArticle(self._template_dir)
         article.load_from_file(file_path)
-        self.db_control.insert_article(article)
-        self.db_control.close_connect()
+        self._db_control.insert_article(article)
+        self._db_control.close_connect()
