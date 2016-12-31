6a5c9ccf0bd2582cf42577712309b8fd6e912966
Akira Ota 10nin <akira19870221@gmail.com>
2016-12-31 17:33:31 +0900

    Add replace double quotation mark from configuration file parameters.

--- blo/__init__.py
+++ blo/__init__.py
@@ -7,9 +7,9 @@ class Blo:
     def __init__(self, config_file_path):
         config = configparser.ConfigParser()
         config.read(config_file_path)
-        self._db_file_path = config['DB']['DB_PATH']
-        self._template_dir = config['TEMPLATE']['TEMPLATE_DIR']
-        self._default_template_file = config['TEMPLATE']['DEFAULT_TEMPLATE_FILE']
+        self._db_file_path = config['DB']['DB_PATH'].replace('"', '')
+        self._template_dir = config['TEMPLATE']['TEMPLATE_DIR'].replace('"', '')
+        self._default_template_file = config['TEMPLATE']['DEFAULT_TEMPLATE_FILE'].replace('"', '')
 
         # create tables
         self._db_control = DBControl(self._db_file_path)
